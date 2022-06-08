001e3a64d2c0187b62107dcda8f6255f24bc9ed6
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-06-08 23:45:45 +0300

    s
    s

--- register_1.php
+++ register_1.php
@@ -132,72 +132,7 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script>const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const repassword = document.getElementById('repassword');
-
-
-function error(input, message) {
-    input.className = 'form-control is-invalid';
-    const div = input.nextElementSibling;
-    div.innerText = message;
-    div.className = 'invalid-feedback';
-}
-
-function success(input) {
-    input.className = 'form-control is-valid';
-}
-
-function checkEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    if (re.test(input.value)) {
-        success(input);
-    } else {
-        error(input, 'Wrong type');
-    }
-}
-
-function check_required(inputs) {
-    inputs.forEach(function (input) {
-        if (input.value === '') {
-            error(input, `${input.id} is required.`);
-        } else {
-            success(input);
-        }
-    });
-}
-
-
-function check_length(input, min, max) {
-    if (input.value.length < min) {
-        error(input, ` This side must be minimum ${min} characther.`);
-    } else if (input.value.length > max) {
-        error(input, `This side must be maximum ${max} characther.`);
-    } else {
-        success(input);
-    }
-}
-
-function check_password(pword, repword) {
-    if (pword.value !== repword.value) {
-        error(repassword, "Passwords are not match!");
-    }
-}
-
-
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    check_required([username, email, password, repassword]);
-    checkEmail(email);
-    check_length(username, 5, 21);
-    check_length(password, 5, 21);
-    check_password(password, repassword);
-}); </script>
+    <script> </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 
